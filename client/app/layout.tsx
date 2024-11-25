6ae68e2797484702747e14e5965b115a782c381e
KetanGupta05 <gketan86@gmail.com>
2024-11-26 03:23:25 +0530

    authentication setup

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,16 +1,11 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
+import { Toaster } from "react-hot-toast";
+import UserProvider from "@/providers/UserProvider";
+import {Inter} from "next/font/google";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const inter = Inter({
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
@@ -25,11 +20,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Toaster position="top-center"/>
+      <UserProvider>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={inter.className}
       >
         {children}
       </body>
+      </UserProvider>
     </html>
   );
 }
